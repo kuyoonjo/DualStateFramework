f4f122250736cad987c62d8511dfef44eb11efd1
kuyoonjo <yuchen@MacBook-Pro.lan>
2014-12-02 18:27:43 +0000

    clear unstaged files

--- src/dsf/Synchronized.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Synchronized.cpp
-//  dsf
-//
-//  Created by Yu Chen on 12/2/14.
-//
-//
-
-#include <stdio.h>
--- src/dsf/SynchronizedVar.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  SynchronizedVar.cpp
-//  dsf
-//
-//  Created by Yu Chen on 12/2/14.
-//
-//
-
-#include <stdio.h>
