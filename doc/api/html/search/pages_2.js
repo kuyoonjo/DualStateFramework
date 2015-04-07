d91764e46b0290317c81bbe083279e405f35a6a5
Yu Chen <yu.chen@live.ie>
2015-04-07 06:46:26 +0100

    Design, Research, API, Functional Specification Documents done.

--- doc/api/html/search/pages_2.js
+++ doc/api/html/search/pages_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['mac_20os_20x',['Mac os X',['../_mac.html',1,'']]],
+  ['mac_20os_20x',['Mac OS X',['../_mac.html',1,'']]],
   ['microsoft_20windows',['Microsoft Windows',['../_win.html',1,'']]]
 ];
